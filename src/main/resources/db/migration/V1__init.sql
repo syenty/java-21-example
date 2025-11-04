9e6971cef88bd9ac26c526083685aa3181c3b746
syenty <tog1993@naver.com>
2025-11-04 20:44:23 +0900

    init commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,26 @@
+-- 사용자 테이블
+CREATE TABLE user
+(
+    id                BIGINT AUTO_INCREMENT NOT NULL,
+    name              VARCHAR(255)          NOT NULL,
+    phone_number      VARCHAR(255)          NOT NULL,
+    organization_code VARCHAR(255)          NOT NULL,
+    CONSTRAINT pk_user PRIMARY KEY (id)
+);
+
+ALTER TABLE user
+    ADD CONSTRAINT UQ_USER_PHONE_NUMBER UNIQUE (phone_number);
+
+-- 어드민 테이블
+CREATE TABLE admin
+(
+    id       BIGINT AUTO_INCREMENT NOT NULL,
+    name     VARCHAR(255)          NOT NULL,
+    email    VARCHAR(255)          NOT NULL,
+    password VARCHAR(255)          NOT NULL,
+    role     VARCHAR(255)          NOT NULL,
+    CONSTRAINT pk_admin PRIMARY KEY (id)
+);
+
+ALTER TABLE admin
+    ADD CONSTRAINT UQ_ADMIN_EMAIL UNIQUE (email);
