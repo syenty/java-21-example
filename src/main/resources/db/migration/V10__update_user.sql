ed3cdd45399b0866d1a0d1bebc41c93192826cee
syenty <syenty0910@imform.co.kr>
2025-11-13 10:06:42 +0900

    feat: update flyway

--- src/main/resources/db/migration/V10__update_user.sql
+++ src/main/resources/db/migration/V10__update_user.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `user`
     ADD COLUMN branch_code VARCHAR(50) NULL AFTER employee_number,
-    DROP INDEX `UK_user_phone_number`,
+    DROP INDEX `UQ_USER_PHONE_NUMBER`,
     ADD UNIQUE KEY `uk_user_name_employee_number` (`name`, `employee_number`);
